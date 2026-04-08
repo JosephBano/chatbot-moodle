80f18c68b2ff27354c93045f05e2d5d087ead8e0
Joseph Bano <andres.jbo2003@gmail.com>
2026-04-08 13:53:16 -0500

    fix: docs updated

--- moodle-docker/config.php
+++ moodle-docker/config.php
@@ -4,23 +4,23 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'mariadb';
+$CFG->dbtype = 'mariadb';
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'moodle_db';
-$CFG->dbname    = 'moodle_db';
-$CFG->dbuser    = 'moodle_user';
-$CFG->dbpass    = 'moodle_pass';
-$CFG->prefix    = 'mdl_';
-$CFG->dboptions = array (
+$CFG->dbhost = 'moodle_db';
+$CFG->dbname = 'moodle_db';
+$CFG->dbuser = 'moodle_user';
+$CFG->dbpass = 'moodle_pass';
+$CFG->prefix = 'mdl_';
+$CFG->dboptions = array(
   'dbpersist' => 0,
-  'dbport'    => '',
-  'dbsocket'  => '',
+  'dbport' => '',
+  'dbsocket' => '',
   'dbcollation' => 'utf8mb4_unicode_ci',
 );
 
-$CFG->wwwroot   = 'http://localhost';
-$CFG->dataroot  = '/var/moodledata';
-$CFG->admin     = 'admin';
+$CFG->wwwroot = 'http://[IP_ADDRESS]:8080';
+$CFG->dataroot = '/var/moodledata';
+$CFG->admin = 'admin';
 
 $CFG->directorypermissions = 02777;
 
